1ba59738b34316a001bdf2e73594e6a7f2bff108
ErikMast <E.Mast@dcterra.nl>
2024-10-03 13:07:44 +0200

    Aanpassingen voor Laravel 11

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,37 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Contact;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $contacts = Contact::all();
-
         return view('contacts.index', compact('contacts'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         return view('contacts.create');
-	}
+    }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -44,38 +36,30 @@ class ContactController extends Controller
         ]);
 
         Contact::create($request->all());
-        return redirect()->route('contacts.index')->with('success', 'Contact is bewaard!');
+        return redirect()->route('contacts.index')
+			  ->with('success', 'Contact is bewaard!');
+
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function show(Contact $contact)
     {
-        return view('contacts.show', compact('contact'));
-    }
+         return view('contacts.show', compact('contact'));
+     }
+
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit(Contact $contact)
     {
-        // $contact = Contact::find($id);
         return view('contacts.edit', compact('contact'));
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Contact $contact)
     {
@@ -87,20 +71,18 @@ class ContactController extends Controller
 
         $contact->update($request->all());
 
-        return redirect('/contacts')->with('success', 'Contact is aangepast!');
+        return redirect('/contacts')
+          ->with('success', 'Contact is aangepast!');
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Contact $contact)
     {
-              // $contact = Contact::find($id);
         $contact->delete();
 
-        return redirect('/contacts')->with('success', 'Contact is verwijderd!');
+        return redirect('/contacts')
+          ->with('success', 'Contact is verwijderd!');
     }
 }
--- database/migrations/2024_09_27_115712_create_contacts_table.php
+++ database/migrations/2024_09_27_115712_create_contacts_table.php
@@ -4,19 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->timestamps();
-			$table->string('first_name');
+            $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
             $table->string('job_title')->nullable();
@@ -27,11 +25,9 @@ class CreateContactsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('contacts');
     }
-}
+};
